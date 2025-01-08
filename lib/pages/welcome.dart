c4a6fe50d939a0b6182f61e8c8d543da46fa69d1
Abishek V S <abhiabsolution23@gmail.com>
2025-01-08 11:41:30 +0530

    changes

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/result_page.dart';
+import 'package:flutter_application_1/widgets/custom_text_field_widget.dart';
 
 class WelcomeApp extends StatelessWidget {
   final String name;
@@ -62,6 +63,7 @@ class WhiteContainer extends StatefulWidget {
 class _WhiteContainerState extends State<WhiteContainer> {
   bool _isFirstButtonSelected = true;
   final weightvalues = <String>['Kgs', 'Lbs'];
+  TextEditingController controller = TextEditingController();
 
   void _selectedFirstButton() {
     setState(() {
@@ -90,192 +92,188 @@ class _WhiteContainerState extends State<WhiteContainer> {
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 80,
+                        backgroundImage: AssetImage('assets/images/male.png'),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CircleAvatar(
+                        radius: 80,
+                        backgroundImage: AssetImage('assets/images/female.png'),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 55,
+                  padding: EdgeInsets.all(8.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromARGB(255, 208, 240, 255),
+                  ),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          _selectedFirstButton();
+                        },
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(
+                          height: 40,
+                          width: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: _isFirstButtonSelected
+                                ? Colors.lightBlueAccent
+                                : Color.fromARGB(255, 208, 240, 255),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Male',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      InkWell(
+                        onTap: () {
+                          _selectSecondButton();
+                        },
+                        borderRadius: BorderRadius.circular(8),
+                        child: Container(
+                          height: 40,
+                          width: 175,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: !_isFirstButtonSelected
+                                ? Colors.lightBlueAccent
+                                : Color.fromARGB(255, 208, 240, 255),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Female',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ),
+                          //color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 10,
+                ),
+                Row(
                   children: [
-                    CircleAvatar(
-                      radius: 80,
-                      backgroundImage: AssetImage('assets/images/male.png'),
+                    Text(
+                      'Age:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                     SizedBox(
                       width: 20,
                     ),
-                    CircleAvatar(
-                      radius: 80,
-                      backgroundImage: AssetImage('assets/images/female.png'),
-                    )
+                    CustomTextField(controller: controller),
                   ],
                 ),
-              ),
-              Container(
-                height: 55,
-                padding: EdgeInsets.all(8.0),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: const Color.fromARGB(255, 208, 240, 255),
+                SizedBox(
+                  height: 40,
                 ),
-                child: Row(
+                Row(
                   children: [
-                    InkWell(
-                      onTap: () {
-                        _selectedFirstButton();
-                      },
-                      borderRadius: BorderRadius.circular(5),
-                      child: Container(
-                        height: 40,
-                        width: 175,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: _isFirstButtonSelected
-                              ? Colors.lightBlueAccent
-                              : Color.fromARGB(255, 208, 240, 255),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Male',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ),
-                      ),
+                    Text(
+                      'Weight:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(width: 10),
-                    InkWell(
-                      onTap: () {
-                        _selectSecondButton();
-                      },
-                      borderRadius: BorderRadius.circular(8),
-                      child: Container(
-                        height: 40,
-                        width: 175,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: !_isFirstButtonSelected
-                              ? Colors.lightBlueAccent
-                              : Color.fromARGB(255, 208, 240, 255),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Female',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ),
-                        //color: Colors.blue,
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 270,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromARGB(255, 208, 240, 255),
+                        //boxShadow: BoxShadow(blurStyle: BlurStyle.normal)
                       ),
+                      child: TextField(),
+                    ),
+                    // DropdownButton(
+                    //     items: List.from(weightvalues), onChanged: null),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Height:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 270,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromARGB(255, 208, 240, 255),
+                        //boxShadow: BoxShadow(blurStyle: BlurStyle.normal)
+                      ),
+                      child: TextField(),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 30,
-                width: 10,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Age:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 40,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color.fromARGB(255, 208, 240, 255),
-                      //boxShadow: BoxShadow(blurStyle: BlurStyle.normal)
-                    ),
-                    child: TextField(),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Weight:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 40,
-                    width: 270,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color.fromARGB(255, 208, 240, 255),
-                      //boxShadow: BoxShadow(blurStyle: BlurStyle.normal)
-                    ),
-                    child: TextField(),
-                  ),
-                  // DropdownButton(
-                  //     items: List.from(weightvalues), onChanged: null),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Height:',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Container(
-                    height: 40,
-                    width: 270,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Color.fromARGB(255, 208, 240, 255),
-                      //boxShadow: BoxShadow(blurStyle: BlurStyle.normal)
-                    ),
-                    child: TextField(),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultPage(),
-                      ));
-                },
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  height: 50,
-                  width: 220,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.lightBlueAccent,
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Calculate your BMI',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                  ),
-                  //color: Colors.blue,
+                SizedBox(
+                  height: 40,
                 ),
-              )
-            ],
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ResultPage(),
+                        ));
+                  },
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    height: 50,
+                    width: 220,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.lightBlueAccent,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Calculate your BMI',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    //color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
